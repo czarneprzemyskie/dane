113c4a35ccbbaa7d1f00f8886707006446b25732
czarneprzemyskie <ozerek36@gmail.com>
2026-01-20 13:55:43 +0100

    Update History.tsx

--- src/components/History.tsx
+++ src/components/History.tsx
@@ -3,17 +3,27 @@ export default function History() {
     <section>
       <h2>Historia</h2>
       <p>
-        Tutaj znajdziesz krótką historię czarnych tablic rejestracyjnych i ich znaczenie dla miasta. Możesz dodać tu rozbudowany tekst, ilustracje oraz skany dokumentów.
+        Tutaj znajdziesz krótką historię czarnych tablic rejestracyjnych na terenie województwa Przemyskiego.
       </p>
-      <h3>Co warto zawrzeć</h3>
+      <h3>WSTĘP:PODZIAŁ ADMINISTRACYJNY WOJEWÓDZTWA PRZEMYSKIEGO (LATA 1975-1998):</h3>
+      <p>Utworzone 1.06.1975 r. woj. przemyskie dzieliło się na następujące miasta, miasta i gminy oraz gminy:</p>
       <ul>
-        <li>Pochodzenie formatu tablic</li>
-        <li>Zmiany administracyjne wpływające na rejestrację</li>
-        <li>Przykłady zabytkowych egzemplarzy</li>
+        <li>miasta: Przemyśl, Jarosław, Lubaczów, Przeworsk.</li> <br>
+        <li>miasta i gminy: Cieszanów, Dynów, Kańczuga, Radymno, Sieniawa, Narol (od 1996r.), Oleszyce (od 1989r.).</li><br>
+        <li>gminy: Adamówka, Bircza, Chłopice, Dubiecko, Fredropol, Gać, Horyniec, Jarosław, Jawornik Polski, Krasiczyn, Krzywcza, Laszki, Lubaczów, Medyka, Narol (od 1996 r. miasto i gmina Narol), Oleszyce (od 1989 r. miasto i gmina Oleszyce), Orły, Pawłosiów, Pruchnik, Przemyśl, Przeworsk, Rokietnica, Roźwienica, Stary Dzików, Stubno, Tryńcza, Wiązownica, Wielkie Oczy, Zarzecze, Żurawica.</li><br>
       </ul>
       <p>
-        Masz materiały (PTR / praktyka)? Wklej linki lub prześlij pliki — mogę pomóc dopisać i uporządkować treść.
+        * W 01.07.1991 r. Podzielono miasto i gminę Dynów oraz miasto i gminę Radymno na dwie jednostki administracyjne.
       </p>
+          <h3>WOJEWÓDZTWO PRZEMYSKIE PO REFORMIE WPROWADZAJĄCEJ 16 WOJEWÓDZTW</h3>
+          <p>01.01.1999 wszedł nowy podział administacyjny wprowadzający 16 województw. Tereny woj. Przemyskiego zostały włączone do nowo powstałego woj, Podkarpackiego, a na jego terenach powstały następujące powiaty:</p>
+          <ul>
+            <li>miasto Przemyśl (grodzki)</li>
+            <li>Przemyski (ziemski) w który wchodziły gminy: Bircza, Dubiecko, Fredropol, Krasiczyn, Krzywcza, Medyka, Orły, Przemyśl, Stubno, Żurawica.</li>
+            <li>Jarosławski w który wchodziły: miasta Jarosław i Radymno, gminy: Chłopice, Jarosław, Laszki, Pawłosiów, Pruchnik, Radymno, Rokietnica, Roźwienica, Wiązownica.</li>
+            <li>Przeworski w który wchodziły miasta i gminy: Kańczuga, Sieniawa, gminy: Adamówka, Gać, Jawornik Polski, Przeworsk, Tryńcza, Zarzecze.</li>
+            <li>Lubaczowski w który wchodziły miasta i gminy: Cieszanów, Narol, Oleszyce, gminy: Horyniec (od 1.01.2002 r. Horyniec-Zdrój), Lubaczów, Stary Dzików, Wielkie Oczy.</li>
+          </ul>
     </section>
   );
 }
